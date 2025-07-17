1f59235271979507192e105aaf2822a35b5a5564
= <davicandidopucminas@gmail.com>
2025-07-17 12:19:52 -0300

    Update crud.js

--- Swagger_with_Jsdoc/src/routes/crud.js
+++ Swagger_with_Jsdoc/src/routes/crud.js
@@ -24,16 +24,97 @@ const router = express.Router();
 
 /** 
  * @openapi
- * /get:
+ * /posts:
+ *  post:
+ *    summary: Cria um post
+ *    description: Essa rota cria um post
+ *    tags: [Posts]
+ *    security:
+ *      - bearerAuth: []
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/post'
+ *          examples:
+ *            post:
+ *              value:
+ *                title: Post 1
+ *                content: Conteudo do post 1
+ *   responses:
+ *     201:
+ *       description: Post criado com sucesso
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             example:
+ *               message: Post created successfully
+ *               post:
+ *                 id: 1
+ *                 title: Post 1
+ *                 content: Conteudo do post 1
+ *     400:
+ *       description: Dados incorretos ou incompletos
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             example:
+ *               message: Title and content are required
+*/
+router.post("/", crudController.save);
+
+
+
+/** 
+ * @openapi
+ * /posts:
  *  get:
- *  
- * 
- */
+ *    summary: Cria um post
+ *    description: Essa rota cria um post
+ *    tags: [Posts]
+ *    security:
+ *      - bearerAuth: []
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/post'
+ *          examples:
+ *            post:
+ *              value:
+ *                title: Post 1
+ *                content: Conteudo do post 1
+ *   responses:
+ *     201:
+ *       description: Post criado com sucesso
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             example:
+ *               message: Post created successfully
+ *               post:
+ *                 id: 1
+ *                 title: Post 1
+ *                 content: Conteudo do post 1
+ *     400:
+ *       description: Dados incorretos ou incompletos
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             example:
+ *               message: Title and content are required
+*/
 router.get("/", crudController.index);
+
 
 router.get("/:id", crudController.show);
 
-router.post("/", crudController.save);
 
 router.put("/:id", crudController.update);
 
